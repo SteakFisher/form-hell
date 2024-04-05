83f2a0c4bf8b06c9321539f82ef64fa0f5bf9532
SteakFisher <miniminter2018p@gmail.com>
2024-04-05 18:37:04 +0530

    Finished input and multiple choice validation Convoluted method for setting "other" when selectMultiple is true btw

--- src/contexts/FormRendererContext.tsx
+++ src/contexts/FormRendererContext.tsx
@@ -1,10 +1,10 @@
 import {createContext} from "react";
-import FormItem from "@/interfaces/FormItem";
+import FormResponse from "@/interfaces/FormResponse";
 
 interface FormRendererContextInterface {
-  formItems: FormItem[]
+  formResponses: FormResponse
 }
 
 export const FormRendererContext = createContext<FormRendererContextInterface>({
-  formItems: []
+  formResponses: {}
 })
