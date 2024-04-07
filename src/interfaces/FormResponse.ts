2de5509b24878a0129466ad01e8e9325fd515734
Razin <razinaj1234@gmail.com>
2024-04-07 19:43:00 +0400

    learn how to spell...

--- src/interfaces/FormResponse.ts
+++ src/interfaces/FormResponse.ts
@@ -1,7 +1,7 @@
-import TextInputResponse from "@/interfaces/form-component-response-interace/TextInputResponse";
-import DropdownResponse from "@/interfaces/form-component-response-interace/DropdownResponse";
-import DateResponse from "@/interfaces/form-component-response-interace/DateResponse";
-import MultipleChoiceResponse from "@/interfaces/form-component-response-interace/MultipleChoiceResponse";
+import TextInputResponse from "@/interfaces/form-component-response-interfaces/TextInputResponse";
+import DropdownResponse from "@/interfaces/form-component-response-interfaces/DropdownResponse";
+import DateResponse from "@/interfaces/form-component-response-interfaces/DateResponse";
+import MultipleChoiceResponse from "@/interfaces/form-component-response-interfaces/MultipleChoiceResponse";
 
 export default interface FormResponse {
   id: string;
