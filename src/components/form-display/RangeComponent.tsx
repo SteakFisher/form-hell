232a6e5d8236e32464d7579d160a7011bdfcca27
Razin <razinaj1234@gmail.com>
2024-05-22 12:49:06 +0400

    Fix slider

--- src/components/form-display/RangeComponent.tsx
+++ src/components/form-display/RangeComponent.tsx
@@ -1,72 +1,133 @@
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {RangeProps} from "@/interfaces/form-component-interfaces/RangeProps";
-import {useContext, useEffect, useState} from "react";
-import {FormRendererContext} from "@/contexts/FormRendererContext";
-import * as SliderPrimitive from "@radix-ui/react-slider";
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { FormRendererContext } from "@/contexts/FormRendererContext";
+import { RangeProps } from "@/interfaces/form-component-interfaces/RangeProps";
 import RangeResponse from "@/interfaces/form-component-response-interfaces/RangeResponse";
+import { cn } from "@/lib/utils";
+import * as SliderPrimitive from "@radix-ui/react-slider";
 import * as React from "react";
-import {cn} from "@/lib/utils";
+import { useContext, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const Slider = React.forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+	React.ElementRef<typeof SliderPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => {
-  const [open, setOpen] = useState(false);
-  return (
-    <SliderPrimitive.Root
-      ref={ref}
-      className={cn(
-        "relative flex w-full touch-none select-none items-center",
-        className,
-      )}
-      {...props}
-    >
-      <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-        <SliderPrimitive.Range className="absolute h-full bg-primary" />
-      </SliderPrimitive.Track>
-      <TooltipProvider>
-        <Tooltip open={open}>
-          <TooltipTrigger asChild>
-            <SliderPrimitive.Thumb onClick={() => setOpen(true)} className="focus-visible:border-1 block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:border-ring focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
-          </TooltipTrigger>
-          <TooltipContent onPointerDownOutside={() => setOpen(false)}>
-            <p>{props.value ? props.value[0] : 0}</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+	const tooltipCloseDelay = 250;
+	const KBTooltipCloseDay = 750;
 
-    </SliderPrimitive.Root>
-  )
+	const [open, setOpen] = useState(false);
+
+	const closeTooltipAfterKBNav = useDebouncedCallback(() => {
+		setOpen(false);
+	}, KBTooltipCloseDay);
+
+	return (
+		<SliderPrimitive.Root
+			ref={ref}
+			className={cn(
+				"relative flex w-full touch-none select-none items-center",
+				className,
+			)}
+			{...props}
+		>
+			<SliderPrimitive.Track
+				onPointerDown={() => {
+					window.addEventListener(
+						"pointerup",
+						() => {
+							setTimeout(() => setOpen(false), tooltipCloseDelay);
+						},
+						{ once: true },
+					);
+					setOpen(true);
+				}}
+				className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20"
+			>
+				<SliderPrimitive.Range className="absolute h-full bg-primary" />
+			</SliderPrimitive.Track>
+			<TooltipProvider>
+				<Tooltip open={open}>
+					<TooltipTrigger asChild>
+						<SliderPrimitive.Thumb
+							onKeyDown={(e) => {
+								if (
+									![
+										"ArrowRight",
+										"ArrowLeft",
+										"ArrowUp",
+										"ArrowDown",
+										"PageUp",
+										"PageDown",
+										"Home",
+										"End",
+									].includes(e.key)
+								)
+									return;
+								setOpen(true);
+								closeTooltipAfterKBNav();
+							}}
+							onPointerDown={() => {
+								window.addEventListener(
+									"pointerup",
+									() => {
+										setTimeout(
+											() => setOpen(false),
+											tooltipCloseDelay,
+										);
+									},
+									{ once: true },
+								);
+								setOpen(true);
+							}}
+							className="focus-visible:border-1 block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:border-ring focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
+						/>
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>{props.value ? props.value[0] : 0}</p>
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</SliderPrimitive.Root>
+	);
 });
 
 Slider.displayName = SliderPrimitive.Root.displayName;
 
-export default function RangeComponent({ props, id } : { id: string; props: RangeProps }) {
-  const { formResponses } = useContext(FormRendererContext)
-  const [val, setVal] = useState<number>(props.min)
-  let rangeResp = formResponses[id] as RangeResponse
+export default function RangeComponent({
+	props,
+	id,
+}: {
+	id: string;
+	props: RangeProps;
+}) {
+	const { formResponses } = useContext(FormRendererContext);
+	const [val, setVal] = useState<number>(props.min);
+	let rangeResp = formResponses[id] as RangeResponse;
 
-  return (
-    <Card className={"w-10/12 self-center mb-4"}>
-      <CardHeader>
-        <CardTitle>
-          {props.title}
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Slider
-          onValueChange={(value) => {
-            formResponses[id] = { range: value[0], type: "range" }
-            setVal(value[0])
-          }}
-          value={[val]}
-          defaultValue={[props.min]}
-          max={props.max}
-          min={props.min}
-          step={props.step}
-        />
-      </CardContent>
-    </Card>
-  )
+	return (
+		<Card className={"mb-4 w-10/12 self-center"}>
+			<CardHeader>
+				<CardTitle>{props.title}</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<Slider
+					onValueChange={(value) => {
+						formResponses[id] = { range: value[0], type: "range" };
+						setVal(value[0]);
+					}}
+					value={[val]}
+					defaultValue={[props.min]}
+					max={props.max}
+					min={props.min}
+					step={props.step}
+				/>
+			</CardContent>
+		</Card>
+	);
 }
