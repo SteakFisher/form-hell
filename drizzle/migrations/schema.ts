2d6b38b077b8644174533af537574cf9857396e2
SteakFisher <miniminter2018p@gmail.com>
2024-06-12 01:59:11 +0400

    Minor changes

--- drizzle/migrations/schema.ts
+++
@@ -1,6 +0,0 @@
-import { sqliteTable, AnySQLiteColumn } from "drizzle-orm/sqlite-core"
-  import { sql } from "drizzle-orm"
-
-export const Hi = sqliteTable("Hi", {
-	Yes: text("Yes", { length: 10 }),
-});
